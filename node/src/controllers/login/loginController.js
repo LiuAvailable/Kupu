299f61480293b11a27c73400915746a2f1d673c3
Liu Santana <lsantana8@boscdelacoma.cat>
2023-04-28 15:54:54 +0200

    api funcionant

---
+++ node/src/controllers/login/loginController.js
@@ -0,0 +1,11 @@
+
+const login = (async (req, res) => {
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+    console.log('AAAA')
+    res.status(200).send({"data":"OK"})
+});
+
+module.exports = { login };
